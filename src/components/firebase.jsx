2d6aa8d9f10d90464ebbbfc1ea34f5f17ba04f12
YWei Chen <b85125@gmail.com>
2025-05-10 17:29:23 +0800

    20250510_1729

---
+++ src/components/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJzG23PuZ1JYTWkl1AZrKmq2k0qcFBJmg",
+  authDomain: "site-facility-e6bb7.firebaseapp.com",
+  projectId: "site-facility-e6bb7",
+  storageBucket: "site-facility-e6bb7.firebasestorage.app",
+  messagingSenderId: "58850898640",
+  appId: "1:58850898640:web:c910d1271ddfc77b0824d5"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,64 @@
+import { useState } from "react";
 import "./regicss.css";
 
+const Button = ({handler}) => {
+    return(<>
+        <button type="submit" className="btn btn-lg btn-dark w-100">
+            {handler ? "登入" : "註冊"}
+        </button>
+    </>)
+}
+
+
+
 export default function Register() {
+    const [handler , setHandler] =useState(true);
+    
+    const onClick1 = () => {
+        var signin = document.querySelector("#signin");
+        var signup = document.querySelector("#signup");
+        signin.style.backgroundColor = "#cbcdd0";
+        signup.style.backgroundColor = "white";
+        setHandler(true);
+    }
+    const onClick2 = () => {
+        var signin = document.querySelector("#signin");
+        var signup = document.querySelector("#signup");
+        signin.style.backgroundColor = "white";
+        signup.style.backgroundColor = "#cbcdd0";
+        setHandler(false);
+    }
+
     return (<>
         <div className="container d-flex flex-column align-items-center justify-content-center vh-100">
             <div className="signBox">
                 <div className="logoFocus text-center my-5 pb-3 w-100">
                     <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/FocusLogo.png' className="img-fluid" alt='' />
+                </div>
+                <div className='chosedBox'>
+                    <div className="inBlock" id="signin" onClick={onClick1}>
+                        <h5>登入</h5>
+                    </div>
+                    <div className="inBlock" id="signup" onClick={onClick2}>
+                        <h5>註冊</h5>
+                    </div>
+                </div>
+                <form className="mb-5">
+                    <div className='form-floating mb-2'>
+                        <input type='username' className="form-control" id="floatingUsername" placeholder="david" />
+                        <label htmlFor="floatingUsername">David</label>
+                    </div>
+                    <div className='form-floating mb-2'>
+                        <input type='email' className="form-control" id="floatingEmail" placeholder="name@example.com" />
+                        <label htmlFor="floatingEmail">Email address</label>
+                    </div>
+                    <div className="form-floating mb-2">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                        <label htmlFor="floatingPassword">Password</label>
+                    </div>
+                </form>
+                <Button handler={handler}></Button>
             </div>
-            <div className='h4 mb-4 text-center'>請輸入帳號密碼</div>
-                <div className='form-floating mb-2'>
-                    <input type='email' className="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating mb-2">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-                <div className="container-fluid d-flex flex-row mb-4 pb-4">
-                    <a href="#" role="button" className="me-auto text-decoration-none">忘記密碼</a>
-                    <a href="#" role="button" className="ms-auto text-decoration-none">註冊</a>
-                </div>
-
-                <button type="submit" className="btn btn-lg btn-dark">登入</button>
-            </div>
-        </div> 
+        </div>
     </>)
 }
