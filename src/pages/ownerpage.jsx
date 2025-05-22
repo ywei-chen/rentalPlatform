ad99828f0e5f558e1e18e3e0d58fb9d54f99fc9f
Kane C.(陳奕瑋) <F19712@pwind.com.tw>
2025-05-22 18:04:44 +0800

    202505221804

--- src/pages/ownerpage.jsx
+++ src/pages/ownerpage.jsx
@@ -3,21 +3,106 @@ import "../ui/ownerpage.css";
 export default function Ownerpage() {
 
     return(<>
-    <div className='container-fluid'>
+    <div className='container-lg'>
         <div className='pictureoutline'>
-            <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-1.jpg' alt=''></img>
-            <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-2.jpg' alt=''></img>
-            <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-3.jpg' alt=''></img>
+                <div className="pic-1">
+                    <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-1.jpg' alt=''></img>
+                </div>
+                <div className="pic-2">
+                    <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-2.jpg' alt=''></img>
+                </div>
+                <div className="pic-3">
+                <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/court-3.jpg' alt=''></img>
+            </div>
         </div>
     </div>
         <div className='container-lg'>
             <div className='contextspace'>
-                <section className='storename&address'></section>
-                <section className='spaceintroduction'></section>
-                <section className='hourtime'></section>
-                <section className='opentime'></section>
+                <section className='storename&address'>
+                    <div>
+                        <div className="storename"><h1>松上羽球館</h1></div>
+                        <div className="address">高雄市左營區文學路265號</div>
+                    </div> 
+                </section>
+                <section className='space&picture'>
+                    <div>
+                        <div className="space"><h1>空間介紹</h1></div>
+                        <div className="picture"></div>           
+                    </div>
+                </section>
+                <section className='hourtime'>
+                    <div>
+                        <div className="title"><h4>開放時段</h4></div>
+                        <table>
+                            <tbody>
+                               <tr>
+                                <td>週一</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週二</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週三</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週四</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週五</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週六</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                                <tr>
+                                <td>週日</td>
+                                <td>
+                                    <div>
+                                        <span>09:00 - 23:00</span>
+                                    </div>
+                                </td>
+                                </tr> 
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
                 <div className='split'></div>
-                <section className='provideditem'></section>
+                <section className='provideditem'>
+                    <div>
+                        <div className="provideditemtext"><h1>提供的設備服務</h1></div>
+                        <div>飲水機</div>
+                        <div>停車場</div>
+                    </div>
+                </section>
             </div>
         </div>
     
