49328163455322c65970cd7776151ae0f88af2ad
Kane C.(陳奕瑋) <F19712@pwind.com.tw>
2025-06-07 17:02:28 +0800

    202506071702

--- src/pages/ownerregister.jsx
+++ src/pages/ownerregister.jsx
@@ -6,42 +6,41 @@ import { firebase } from "../components/firebase";
 import * as bootstrap from 'bootstrap';
 import { Dropdown } from 'react-bootstrap';
 
-const MoneySetting = () => {
-    const [hour , setHour] = useState(null);
-    const [month , setmonth] = useState(null);
-    const [season , setSeason] = useState(null);
-    const [year , setYear] = useState(null);
-    
+const MoneySetting = ({money , setMoney}) => {
+    const handleChange = (key, value) => {
+        setMoney(prev => ({ ...prev, [key]: Number(value) }));
+    };
+
     return (
         <>
             <div className='form-floating mb-2'>
-                <input type='number' className="form-control" id="floatinghour" placeholder="number" onChange={
+                <input type='money' className="form-control" id="floatinghour" placeholder="money" onChange={
                     (e) => {
-                        setHour(e.target.value);
+                        handleChange('hour', e.target.value);
                     }
                 } />
                 <label htmlFor="floatinghour">時租 ex: 300</label>
             </div>
             <div className='form-floating mb-2'>
-                <input type='number' className="form-control" id="floatingmonths" placeholder="number" onChange={
+                <input type='money' className="form-control" id="floatingmonths" placeholder="money" onChange={
                     (e) => {
-                        setmonth(e.target.value);
+                        handleChange('months', e.target.value);
                     }
                 } />
                 <label htmlFor="floatingmonths">月租 ex: 300</label>
             </div>
             <div className='form-floating mb-2'>
-                <input type='number' className="form-control" id="floatingseason" placeholder="number" onChange={
+                <input type='money' className="form-control" id="floatingseason" placeholder="money" onChange={
                     (e) => {
-                        setSeason(e.target.value);
+                        handleChange('season', e.target.value);
                     }
                 } />
                 <label htmlFor="floatingseason">季租 ex: 300</label>
             </div>
             <div className='form-floating mb-2'>
-                <input type='number' className="form-control" id="floatingyear" placeholder="number" onChange={
+                <input type='money' className="form-control" id="floatingyear" placeholder="money" onChange={
                     (e) => {
-                        setYear(e.target.value);
+                         handleChange('year', e.target.value);
                     }
                 } />
                 <label htmlFor="floatingyear">年租 ex: 300</label>
@@ -52,66 +51,76 @@ const MoneySetting = () => {
 }
 
 const CourtSetting = ({ courtCount, setCourtCount }) => {
-  const courtOptions = Array.from({ length: 30 }, (_, i) => i + 1);
+    const courtOptions = Array.from({ length: 30 }, (_, i) => i + 1);
 
-  return (
-    <Dropdown onSelect={(val) => setCourtCount(Number(val))}>
-      <Dropdown.Toggle variant="secondary" id="dropdown-courts">
-        {courtCount ? `${courtCount} 面` : '請選擇場地數量'}
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        {courtOptions.map((num) => (
-          <Dropdown.Item key={num} eventKey={num}>{num} 面</Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+    return (
+        <Dropdown onSelect={(val) => setCourtCount(Number(val))}>
+            <Dropdown.Toggle className="dropdown" variant="" id="dropdown-courts">
+                {courtCount ? `${courtCount} 面` : "請選擇場地數量"}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                {courtOptions.map((num) => (
+                    <Dropdown.Item key={num} eventKey={num}>{num} 面</Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 };
 
 
-const WeekOpentime = ({day}) => {
-    const [startTime, setStartTime] = useState(null);
-    const [endTime, setEndTime] = useState(null);
-    const opentime = Array.from({ length: 13 }, (_, i) => i + 10);
-    const endTimeOption = opentime.filter(hour => startTime === null || hour > Number(startTime));
+const WeekOpentime = ({ weekOpenTimeList, setWeekOpenTimeList }) => {
+    const weekDays = ['Mon.', 'Tue.', 'Wed.', 'Thr.', 'Fri.', 'Sat.', 'Sun.'];
+    const changetime = (dayIndex, timeType, value) => {
+        const update = [...weekOpenTimeList];
+        update[dayIndex][timeType] = value;
+        setWeekOpenTimeList(update);
+    }
 
     return (
         <>
-            <div className="dailyslesct">
-                <div className="dayblock">{day}</div>
-                <div className="date">
-                    <Dropdown onSelect={(hour) => {
-                        setStartTime(Number(hour));
-                        setEndTime(null);
-                    }}>
-                        <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-${day}-start`}>
-                            {startTime === null ? '開始時間' : `${startTime}:00`}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {opentime.map((hour, index) => {
-                                return (
-                                    <Dropdown.Item key={index} eventKey={hour}>{hour}:00</Dropdown.Item>
-                                )
-                            })}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-                <div className="time">
-                    <Dropdown onSelect={(hour) => {
-                        setEndTime(Number(hour));
-                    }}>
-                        <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-${day}-end`}>
-                            {endTime === null ? '結束時間' : `${endTime}:00`}
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {endTimeOption.map((hour, index) => {
-                                return (
-                                    <Dropdown.Item key={index} eventKey={hour}>{hour}:00</Dropdown.Item>
-                                )
-                            })}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
+            <div>
+                {weekOpenTimeList.map((item, index) => {
+                    const opentime = Array.from({ length: 24 }, (_, i) => i + 1);
+                    const endTimeOption = opentime.filter(hour => item.open === "" || hour > Number(item.open));
+                    return (
+                        <div key={index}  className="openslesct">
+                            <div className="dayblock">{weekDays[index]}</div>
+                            <div className="date">
+                                <Dropdown onSelect={(hour) => {
+                                    changetime(index, 'open', hour);
+                                    changetime(index, 'close', '');
+                                }}>
+                                    <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-start-${index}`}>
+                                        {item.open ? `${item.open}:00` : '開始時間'}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        {opentime.map((hour, index) => {
+                                            return (
+                                                <Dropdown.Item key={index} eventKey={hour}>{hour}:00</Dropdown.Item>
+                                            )
+                                        })}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </div>
+                            <div className="time">
+                                <Dropdown onSelect={(hour) => {
+                                    changetime(index, 'close', hour)
+                                }}>
+                                    <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-end-${index}`}>
+                                        {item.close ? `${item.close}:00` : '結束時間'}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        {endTimeOption.map((hour, index) => {
+                                            return (
+                                                <Dropdown.Item key={index} eventKey={hour}>{hour}:00</Dropdown.Item>
+                                            )
+                                        })}
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
             <div className="spllit"></div>
         </>
@@ -124,17 +133,31 @@ export default function Ownerregister() {
     const navigate = useNavigate();
     const modalRef = useRef(null);
     const [handler, setHandler] = useState(1);
-    const [name , setName] = useState('');
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [modalContent, setModalContent] = useState('');
-    const [courtCount, setCourtCount] = useState(1);
-    const weekDays = ['Mon.', 'Tue.' , 'Wed.' , 'Thr.' , 'Fri.' , 'Sat.' , 'Sun.'];
-    
+    const [courtCount, setCourtCount] = useState(null);
+    const [money, setMoney] = useState({
+        hour: 0,
+        months: 0,
+        season: 0,
+        year: 0
+    });
+    const [weekOpenTimeList, setWeekOpenTimeList] = useState([
+        { open: '', close: '' },
+        { open: '', close: '' },
+        { open: '', close: '' },
+        { open: '', close: '' },
+        { open: '', close: '' },
+        { open: '', close: '' },
+        { open: '', close: '' }
+    ]);
+
 
     useEffect(() => {
         myModal = new bootstrap.Modal(modalRef.current);
-    },[])
+    }, [])
 
     const onClickSignIn = () => {
         var signin = document.querySelector("#signin");
@@ -152,46 +175,46 @@ export default function Ownerregister() {
     }
 
     const showLoginError = (e) => {
-        if(e.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS){
+        if (e.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS) {
             setModalContent("帳號密碼錯誤，請重新輸入");
             myModal.show();
         }
-        else if(e.code === AuthErrorCodes.EMAIL_EXISTS){
+        else if (e.code === AuthErrorCodes.EMAIL_EXISTS) {
             setModalContent("帳號已註冊，請重新輸入");
             myModal.show();
         }
-        else{
+        else {
             setModalContent("登入錯誤，請重新輸入");
             myModal.show();
         }
     }
 
     const onSubmit = () => {
-        if (handler === 1){
+        if (handler === 1) {
             const auth = getAuth(firebase);
             signInWithEmailAndPassword(auth, email, password)
-            .then(() => {
-                setModalContent("登入成功，即將跳轉至首頁");
-                myModal.show();
-                setTimeout(() => {
-                    myModal.hide();
-                    navigate('/siteFalicyCRADemo/');
-                }, 3000)
-            })
-            .catch((e) => {
-                showLoginError(e);
-            })
+                .then(() => {
+                    setModalContent("登入成功，即將跳轉至首頁");
+                    myModal.show();
+                    setTimeout(() => {
+                        myModal.hide();
+                        navigate('/siteFalicyCRADemo/');
+                    }, 3000)
+                })
+                .catch((e) => {
+                    showLoginError(e);
+                })
         }
-        else if(handler === 0){
+        else if (handler === 0) {
             const auth = getAuth(firebase);
             createUserWithEmailAndPassword(auth, email, password)
                 .then(() => {
                     setModalContent("註冊成功，，即將跳轉至首頁");
                     myModal.show();
                     setTimeout(() => {
-                    myModal.hide();
-                    navigate('/siteFalicyCRADemo/');
-                }, 3000)
+                        myModal.hide();
+                        navigate('/siteFalicyCRADemo/');
+                    }, 3000)
                 })
                 .catch((e) => {
                     showLoginError(e);
@@ -200,13 +223,13 @@ export default function Ownerregister() {
     }
 
     return (<>
-        <div className="container d-flex flex-column align-items-center justify-content-center vh-100">
+        <div className="container d-flex justu">
             <div className="signBox">
                 <div className="logoFocus text-center my-5 pb-3 w-100">
                     <img src='https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/FocusLogo.png' className="img-fluid" alt='' />
                 </div>
                 <div className='chosedBox'>
-                    <div className="inBlock" id="signin" onClick={onClickSignIn} style={{backgroundColor: "#cbcdd0"}}>
+                    <div className="inBlock" id="signin" onClick={onClickSignIn} style={{ backgroundColor: "#cbcdd0" }}>
                         <h5>登入</h5>
                     </div>
                     <div className="inBlock" id="signup" onClick={onClickSignUp}>
@@ -255,7 +278,7 @@ export default function Ownerregister() {
                         </div>
                         <div className="form-floating mb-2">
                             <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={
-                                (e) => { 
+                                (e) => {
                                     setPassword(e.target.value);
                                     console.log(password);
                                 }
@@ -265,13 +288,7 @@ export default function Ownerregister() {
                         <div className="opentimeset">
                             <div className='opentimeTopic'>-營業時間-</div>
                             <div className="spllit"></div>
-                            {
-                                weekDays.map((day, index) => {
-                                    return (
-                                        <WeekOpentime day={day} key={index}></WeekOpentime>
-                                    )
-                                })
-                            }
+                            <WeekOpentime weekOpenTimeList={weekOpenTimeList} setWeekOpenTimeList={setWeekOpenTimeList}></WeekOpentime>
                         </div>
                         <div className="opentimeset">
                             <div className='opentimeTopic'>-場地數量-</div>
@@ -281,12 +298,12 @@ export default function Ownerregister() {
                         <div className="opentimeset">
                             <div className='opentimeTopic'>-場地租金-</div>
                             <div className="spllit"></div>
-                            <MoneySetting></MoneySetting>
+                            <MoneySetting money={money} setMoney={setMoney}></MoneySetting>
                         </div>
-                        
+
                         <button type="button" className="btn btn-lg btn-dark w-100 mt-5" onClick={onSubmit}>{handler ? "登入" : "註冊"}</button>
                     </form>)
-                    
+
                 }
             </div>
             <div className="modal fade" id="exampleModal" tabIndex="-1" ref={modalRef}>
