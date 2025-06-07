8772e14427e6501603272644cfeffa2dd6c2b5ad
Kane C.(陳奕瑋) <F19712@pwind.com.tw>
2025-06-07 13:25:00 +0800

    202506071324

--- src/pages/ownerregister.jsx
+++ src/pages/ownerregister.jsx
@@ -6,6 +6,73 @@ import { firebase } from "../components/firebase";
 import * as bootstrap from 'bootstrap';
 import { Dropdown } from 'react-bootstrap';
 
+const MoneySetting = () => {
+    const [hour , setHour] = useState(null);
+    const [month , setmonth] = useState(null);
+    const [season , setSeason] = useState(null);
+    const [year , setYear] = useState(null);
+    
+    return (
+        <>
+            <div className='form-floating mb-2'>
+                <input type='number' className="form-control" id="floatinghour" placeholder="number" onChange={
+                    (e) => {
+                        setHour(e.target.value);
+                    }
+                } />
+                <label htmlFor="floatinghour">時租 ex: 300</label>
+            </div>
+            <div className='form-floating mb-2'>
+                <input type='number' className="form-control" id="floatingmonths" placeholder="number" onChange={
+                    (e) => {
+                        setmonth(e.target.value);
+                    }
+                } />
+                <label htmlFor="floatingmonths">月租 ex: 300</label>
+            </div>
+            <div className='form-floating mb-2'>
+                <input type='number' className="form-control" id="floatingseason" placeholder="number" onChange={
+                    (e) => {
+                        setSeason(e.target.value);
+                    }
+                } />
+                <label htmlFor="floatingseason">季租 ex: 300</label>
+            </div>
+            <div className='form-floating mb-2'>
+                <input type='number' className="form-control" id="floatingyear" placeholder="number" onChange={
+                    (e) => {
+                        setYear(e.target.value);
+                    }
+                } />
+                <label htmlFor="floatingyear">年租 ex: 300</label>
+            </div>
+        </>
+
+    )
+}
+
+const CourtSetting = () => {
+    const [totalCourt, setTotalCourt] = useState(null);
+    const courtcount = Array.from({ length: 30 }, (_, i) => i + 1);
+
+    return (
+        <Dropdown onSelect={(item) => {
+            setTotalCourt(Number(item));
+        }}>
+            <Dropdown.Toggle className="dropdown" variant="" id='dropdown-court'>
+                {totalCourt === null ? '請選擇場地數量' : `${totalCourt}面`}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                {courtcount.map((item, index) => {
+                    return (
+                        <Dropdown.Item key={index} eventKey={item}>{item}面</Dropdown.Item>
+                    )
+                })}
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
+
 const WeekOpentime = ({day}) => {
     const [startTime, setStartTime] = useState(null);
     const [endTime, setEndTime] = useState(null);
@@ -21,7 +88,7 @@ const WeekOpentime = ({day}) => {
                         setStartTime(Number(hour));
                         setEndTime(null);
                     }}>
-                        <Dropdown.Toggle variant="" id={`dropdown-${day}-start`}>
+                        <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-${day}-start`}>
                             {startTime === null ? '開始時間' : `${startTime}:00`}
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
@@ -37,7 +104,7 @@ const WeekOpentime = ({day}) => {
                     <Dropdown onSelect={(hour) => {
                         setEndTime(Number(hour));
                     }}>
-                        <Dropdown.Toggle variant="" id={`dropdown-${day}-end`}>
+                        <Dropdown.Toggle className="dropdown" variant="" id={`dropdown-${day}-end`}>
                             {endTime === null ? '結束時間' : `${endTime}:00`}
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
@@ -61,6 +128,7 @@ export default function Ownerregister() {
     const navigate = useNavigate();
     const modalRef = useRef(null);
     const [handler, setHandler] = useState(1);
+    const [name , setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [modalContent, setModalContent] = useState('');
@@ -87,11 +155,11 @@ export default function Ownerregister() {
     }
 
     const showLoginError = (e) => {
-        if(e.code == AuthErrorCodes.INVALID_LOGIN_CREDENTIALS){
+        if(e.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS){
             setModalContent("帳號密碼錯誤，請重新輸入");
             myModal.show();
         }
-        else if(e.code == AuthErrorCodes.EMAIL_EXISTS){
+        else if(e.code === AuthErrorCodes.EMAIL_EXISTS){
             setModalContent("帳號已註冊，請重新輸入");
             myModal.show();
         }
@@ -171,10 +239,10 @@ export default function Ownerregister() {
                 </form>) :
                     (<form method="post" target="_self" onSubmit={onSubmit}>
                         <div className='form-floating mb-2'>
-                            <input type='email' className="form-control" id="floatingName" placeholder="name" onChange={
+                            <input type='name' className="form-control" id="floatingName" placeholder="name" onChange={
                                 (e) => {
-                                    setEmail(e.target.value);
-                                    console.log(email);
+                                    setName(e.target.value);
+                                    console.log(name);
                                 }
                             } />
                             <label htmlFor="floatingname">ex: 松上羽球館</label>
@@ -197,16 +265,28 @@ export default function Ownerregister() {
                             } />
                             <label htmlFor="floatingPassword">Password</label>
                         </div>
-                        <div className='opentimeTopic'>-營業時間-</div>
-                        <div className="spllit"></div>
-                        {
-                            weekDays.map((day, index) => {
-                                return (
-                                    <WeekOpentime day={day} key={index}></WeekOpentime>
-                                )
-                            })
-
-                        }
+                        <div className="opentimeset">
+                            <div className='opentimeTopic'>-營業時間-</div>
+                            <div className="spllit"></div>
+                            {
+                                weekDays.map((day, index) => {
+                                    return (
+                                        <WeekOpentime day={day} key={index}></WeekOpentime>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="opentimeset">
+                            <div className='opentimeTopic'>-場地數量-</div>
+                            <div className="spllit"></div>
+                            <CourtSetting></CourtSetting>
+                        </div>
+                        <div className="opentimeset">
+                            <div className='opentimeTopic'>-場地租金-</div>
+                            <div className="spllit"></div>
+                            <MoneySetting></MoneySetting>
+                        </div>
+                        
                         <button type="button" className="btn btn-lg btn-dark w-100 mt-5" onClick={onSubmit}>{handler ? "登入" : "註冊"}</button>
                     </form>)
                     
@@ -216,7 +296,7 @@ export default function Ownerregister() {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">登入提示</h1>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">{handler ? "登入提示" : "註冊提示"}</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
