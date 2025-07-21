c47442c024f1917b6169ec726b606dfbdfeba208
Kane C. <b85125@gmail.com>
2025-07-21 17:21:06 +0800

    20250721_1721

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,55 +49,52 @@ export default function NavBar() {
     return (
         <Navbar expand="lg" bg="light" variant="light" className="navbar">
             <Container fluid>
-                <Navbar.Brand href="#" className="d-flex align-items-center">
+                <Navbar.Brand href="#" className="logoNav">
                     <img
                         src="https://raw.githubusercontent.com/ywei-chen/siteFalicyCRADemo/refs/heads/main/src/assets/FocusLogo.png"
                         alt="logo"
-                        className="img-fluid object-fit-cover"
-                        style={{ height: '40px' }}
+                        className="imgLogo"
                     />
-                    <span className="ms-2">羽毛球｜擇已所愛</span>
+                    <span className="logoText">羽毛球｜擇已所愛</span>
                 </Navbar.Brand>
-
-                <div className="ms-auto me-3 d-flex align-items-center">
-                    {userName ? (<>
-                        <div>
-                            {userName.name}
+                <div className="loginLogo">
+                    {userName ? (
+                        <div className="d-flex align-items-center me-3">
+                            <span>{userName.name}</span>
                             <i className="fa-regular fa-circle-user fa-xl ms-2"></i>
                         </div>
-                        <Navbar.Toggle aria-controls="navbar-nav" />
-                        <Navbar.Collapse id="navbar-nav">
-                            <Nav className="my-auto">
-                                <Nav.Link as={Link} onClick={handleLogout}>登出</Nav.Link>
-                                <Nav.Link as={Link} to="/siteFalicyCRADemo/">首頁</Nav.Link>
-                                {userName.role === 'user' && (
-                                    <Nav.Link as={Link} to={`/siteFalicyCRADemo/userpage/${userName.uid}`}>個人頁面</Nav.Link>
-                                )}
-                                {userName.role === 'store' && (
-                                    <Nav.Link as={Link} to={`/siteFalicyCRADemo/ownerpage/${userName.uid}`}>商家頁面</Nav.Link>
-                                )}
-                            </Nav>
-                        </Navbar.Collapse>
-                    </>
                     ) : (
-                            <>
-                                <div
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={() => navigate('/siteFalicyCRADemo/userregister/')}
-                                >
-                                    未登入 <i className="fa-regular fa-circle-user fa-xl ms-2"></i>
-                                </div>
-                                <Navbar.Toggle aria-controls="navbar-nav" />
-                                <Navbar.Collapse id="navbar-nav">
-                                    <Nav className="my-auto">
-                                        <Nav.Link as={Link} to="/siteFalicyCRADemo/">首頁</Nav.Link>
-                                        <Nav.Link as={Link} to="/siteFalicyCRADemo/userregister/">登入/註冊</Nav.Link>
-                                        <Nav.Link as={Link} to="/siteFalicyCRADemo/ownerregister/">商家登入/註冊</Nav.Link>
-                                    </Nav>
-                                </Navbar.Collapse>
-                            </>         
+                        <div
+                            onClick={() => navigate('/siteFalicyCRADemo/userregister/')}
+                            className="d-flex align-items-center me-3"
+                        >
+                            <span>未登入</span>
+                            <i className="fa-regular fa-circle-user fa-xl ms-2"></i>
+                        </div>
                     )}
                 </div>
+                    <Navbar.Toggle aria-controls="navbar-nav" />
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/siteFalicyCRADemo/">首頁</Nav.Link>
+                            {userName ? (
+                                <>
+                                    <Nav.Link onClick={handleLogout}>登出</Nav.Link>
+                                    {userName.role === 'user' && (
+                                        <Nav.Link as={Link} to={`/siteFalicyCRADemo/userpage/${userName.uid}`}>個人頁面</Nav.Link>
+                                    )}
+                                    {userName.role === 'store' && (
+                                        <Nav.Link as={Link} to={`/siteFalicyCRADemo/ownerpage/${userName.uid}`}>商家頁面</Nav.Link>
+                                    )}
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link as={Link} to="/siteFalicyCRADemo/userregister/">登入/註冊</Nav.Link>
+                                    <Nav.Link as={Link} to="/siteFalicyCRADemo/ownerregister/">商家登入/註冊</Nav.Link>
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
             </Container>
         </Navbar>
     );
